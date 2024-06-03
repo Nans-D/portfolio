3bcc0153e3f04264d1d9fd1e9935634c8da4e1b4
Nans-D <ndelaubert@gmail.com>
2024-06-03 09:42:07 +0200

    suppression de style body pour réassembler, ajout du début de la section compétences techniques (début), ajout de disabled si un lien est cliqué

--- includes/home.php
+++ includes/home.php
@@ -23,6 +23,25 @@
         </li>
     </ul>
     <h2 class="fs-3 text-decoration-underline">Compétences Techniques</h2>
+    <div class="row">
+        <div class="col-auto">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
+                </div>
+            </div>
+        </div>
+        <div class="col-auto">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 `
--- includes/index.php
+++ includes/index.php
@@ -3,14 +3,14 @@
 <!-- Content -->
 <div class="container-fluid">
     <div class="wrapper position-relative overflow-hidden">
-        <div class="d-flex position-relative flex-column text-light h-100 wrapper-child p-3">
+        <div class="d-flex position-relative flex-column text-light h-100 wrapper-child pt-3 px-3">
             <?php include './waveform.php' ?>
 
             <div class="col-12">
                 <div class='row justify-content-between'>
                     <div class="col">
                         <h1 style="font-size:52px;">Nans DELAUBERT</h1>
-                        <h2 style="font-size:14px;">Full stack developer</h2>
+                        <h2 style="font-size:18px;">Full stack developer</h2>
                     </div>
                     <div id="nameTargetLink" class="col-auto">
 
@@ -34,13 +34,13 @@
                 </ul>
             </nav>
 
-            <div class='row mt-5'>
+            <div class="container-print row mt-5">
                 <div class="col-3"></div>
                 <div class="col-9">
                     <section class="print-section"></section>
                 </div>
             </div>
-            <div id="socialMediaPrint" class="position-relative align-self-end mt-auto d-none z-2">
+            <div id="socialMediaPrint" class="position-relative align-self-end mt-auto d-none z-2 pb-3">
                 <a class="text-light text-decoration-none" target="_blank" href="https://fr.linkedin.com/in/nans-delaubert">
                     <i class="fa-xl fa-brands fa-linkedin"></i>
                 </a>
@@ -62,17 +62,16 @@
             originalTexts[$(this).data('link')] = $(this).text();
         });
 
-        $('.nav-link').on('click', function() {
-            $('#nameTargetLink').text($(this).data('link'));
+        $('.nav-link').on('mouseenter', function() {
+            $(this).addClass('background-link-white');
+            $(this).removeClass('link-light');
         });
 
-        $('.nav-link').on('mouseenter', function() {
-            $(this).addClass('background-link-white')
-            $(this).removeClass('link-light')
-        });
         $('.nav-link').on('mouseleave', function() {
-            $(this).removeClass('background-link-white')
-            $(this).addClass('link-light')
+            if (!$(this).hasClass('active')) { // Ne pas retirer les classes si le lien est actif
+                $(this).removeClass('background-link-white');
+                $(this).addClass('link-light');
+            }
         });
 
         let form = <?php include './contact.php'; ?>;
@@ -82,42 +81,55 @@
             e.preventDefault();
             e.stopPropagation();
 
-            // Reset text for all nav links
+            if ($(this).hasClass('disabled')) {
+                return;
+            }
+
+            $('#nameTargetLink').text($(this).data('link'));
+
+            // Remettre les textes originaux et retirer les classes des autres liens
             $('.nav-link').each(function() {
                 $(this).html(originalTexts[$(this).data('link')]);
+                $(this).removeClass('background-link-white active disabled');
+                $(this).addClass('link-light');
             });
 
-            // Set active link and change its text
-            $('.nav-link').removeClass('active');
-            $(this).addClass('active').html('<i class="fa-solid fa-arrow-right"></i>');
+            // Définir le lien actuel comme actif et désactivé, puis changer son texte
+            $(this).addClass('active disabled').html('<i class="fa-solid fa-arrow-right"></i>').removeClass('link-light');
+
             let data = $(this).data('link');
             switch (data) {
                 case 'Home':
                     $('.print-section').empty().append(home);
-                    $('#socialMediaPrint').removeClass('d-none')
+                    $('#socialMediaPrint').addClass('d-none');
                     break;
                 case 'Contact':
                     $('.print-section').empty().append(form);
-                    $('#socialMediaPrint').addClass('d-none');
-
+                    $('#socialMediaPrint').removeClass('d-none');
                     break;
                 case 'Projects':
                     $('.print-section').empty();
                     $('#socialMediaPrint').addClass('d-none');
-
+                    break;
+                case 'Side':
+                    $('.print-section').empty();
+                    $('#socialMediaPrint').addClass('d-none');
                     break;
             }
         });
+
         $(document).on('mouseenter', '.experience-link', function() {
             $(this).append(`<div class="dropdown-experience my-3">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas 
-            integer eget. Augue eget arcu dictum varius duis at consectetur lorem donec. Risus viverra adipiscing at in tellus integer feugiat scelerisque. Vitae congue eu 
-            consequat ac felis donec et odio
-            </div>`)
-        })
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas 
+    integer eget. Augue eget arcu dictum varius duis at consectetur lorem donec. Risus viverra adipiscing at in tellus integer feugiat scelerisque. Vitae congue eu 
+    consequat ac felis donec et odio
+    </div>`);
+        });
+
         $(document).on('mouseleave', '.experience-link', function() {
-            $('.dropdown-experience').remove()
-        })
+            $('.dropdown-experience').remove();
+        });
+
         $(document).on('submit', '#formContact', function(e) {
             e.preventDefault();
             let formData = $(this).serialize();
